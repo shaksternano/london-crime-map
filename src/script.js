9cfda39a779d009aed827607e83c86e7ef602e55
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-04-20 22:07:11 +0100

    feat: display crime data on map

--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
 "use strict";
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+const crimeData = await fetch("./london_crime_data.json")
+    .then(response => response.json());
+const boroughCrimes = crimeData.dates["2021-04-01"].boroughs
+
+const offencesUpperBound = 10000;
+for (const [borough, boroughData] of Object.entries(boroughCrimes)) {
+    if (borough === "Unknown") {
+        continue;
+    }
+
+    const boroughId = borough.replaceAll(" ", "-").toLowerCase();
+    const boroughElement = document.getElementById(boroughId);
+    if (boroughElement === null) {
+        console.error(`Element with id ${boroughId} not found`);
+        continue
+    }
+
+    const ratio = boroughData.total_criminal_offences / offencesUpperBound;
+    const colorValue = Math.floor(255 * (1 - ratio));
+    const colorHex = colorValue.toString(16).padStart(2, "0");
+    boroughElement.style.fill = `#ff${colorHex}${colorHex}`;
+
+    if (boroughId === "southwark") {
+        const cityOfLondonElement = document.getElementById("city-of-london");
+        cityOfLondonElement.style.fill = boroughElement.style.fill;
+    }
+}

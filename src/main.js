7ee513d68185eaea85b686c4fae8cfc0fbf9f85e
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-04-21 22:35:45 +0100

    feat: add borough data pie chart

--- src/main.js
+++ src/main.js
@@ -28,6 +28,19 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
  * @property {Object.<string, number>} offence_subgroups
  */
 
+const CAPITALIZE_EXCEPTIONS = [
+    "a",
+    "an",
+    "and",
+    "of",
+    "in",
+    "on",
+    "for",
+    "the",
+    "to",
+    "with",
+]
+
 async function main() {
     /**
      * @type {CrimeData}
@@ -42,16 +55,27 @@ async function main() {
     setLegend(offencesUpperBound);
 
     const dates = Object.keys(crimeData.dates);
-    const timelineDateElement = document.getElementById("timeline-date");
 
-    updateData(crimeData, dates[0], offencesUpperBound, timelineDateElement);
+    updateData(crimeData, dates[0], offencesUpperBound);
 
     const timelineSlider = document.getElementById("timeline-slider");
     timelineSlider.max = dates.length - 1;
     timelineSlider.value = 0;
-    timelineSlider.oninput = () => {
-        const date = dates[timelineSlider.value];
-        updateData(crimeData, date, offencesUpperBound, timelineDateElement);
+    timelineSlider.oninput = (event) => {
+        const date = dates[event.target.value];
+        updateData(crimeData, date, offencesUpperBound);
+    }
+
+    const mapElement = document.getElementById("map");
+    for (const boroughElement of mapElement.children) {
+        boroughElement.onclick = () => {
+            const date = dates[timelineSlider.value];
+            let boroughId = boroughElement.id;
+            if (boroughId === "southwark-and-city-of-london") {
+                boroughId = "southwark";
+            }
+            displayBoroughData(crimeData, boroughId, date);
+        }
     }
 }
 
@@ -98,7 +122,10 @@ function setLegend(offencesUpperBound) {
         const ratio = i / legendTiers;
         const previousRatio = (i - 1) / legendTiers;
         const lowerBound = Math.round(offencesUpperBound * previousRatio);
-        const upperBound = Math.round(offencesUpperBound * ratio) - 1;
+        let upperBound = Math.round(offencesUpperBound * ratio);
+        if (i !== legendTiers) {
+            upperBound--;
+        }
         const label = `${lowerBound} – ${upperBound}`;
         const color = getColor(ratio);
 
@@ -133,17 +160,16 @@ function getColor(ratio) {
  * @param crimeData {CrimeData}
  * @param date {string}
  * @param offencesUpperBound {number}
- * @param timelineDateElement {HTMLElement}
  */
 function updateData(
     crimeData,
     date,
     offencesUpperBound,
-    timelineDateElement,
 ) {
     const dateObject = new Date(date);
     const month = dateObject.toLocaleString("default", {month: "long"});
     const year = dateObject.getFullYear();
+    const timelineDateElement = document.getElementById("timeline-date");
     timelineDateElement.innerHTML = `${month} ${year}`;
     displayData(crimeData, date, offencesUpperBound);
 }
@@ -177,6 +203,101 @@ function displayData(
         const ratio = boroughData.total_criminal_offences / offencesUpperBound;
         boroughElement.style.fill = getColor(ratio);
     }
+}
+
+/**
+ * @param crimeData {CrimeData}
+ * @param boroughId {string}
+ * @param date {string}
+ */
+function displayBoroughData(crimeData, boroughId, date) {
+    const boroughData = crimeData.dates[date].boroughs[boroughId];
+    if (boroughData === undefined) {
+        console.error(`Borough data for ${boroughId} on ${date} not found`);
+        return;
+    }
+    const offences = boroughData.total_criminal_offences;
+    const boroughName = boroughId.replaceAll("-", " ")
+        .split(" ")
+        .map(word => {
+            if (CAPITALIZE_EXCEPTIONS.includes(word)) {
+                return word;
+            }
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(" ");
+
+    const dateObject = new Date(date);
+    const month = dateObject.toLocaleString("default", {month: "long"});
+    const year = dateObject.getFullYear();
+
+    const boroughOffenceCountElement = document.getElementById("borough-offence-count");
+    boroughOffenceCountElement.innerHTML = `Criminal offences in ${boroughName} in ${month} ${year}: ${offences}`;
+
+    displayCrimePieChart(crimeData, boroughId, date);
+}
+
+/**
+ * @param crimeData {CrimeData}
+ * @param boroughId {string}
+ * @param date {string}
+ */
+function displayCrimePieChart(crimeData, boroughId, date) {
+    const boroughData = crimeData.dates[date]
+        .boroughs[boroughId]
+        .offence_groups;
+
+    const processedBoroughData = Object.entries(boroughData)
+        .map(([offenceGroup, offenceGroupData]) => {
+            return {
+                offence: offenceGroup,
+                count: offenceGroupData.total_criminal_offences,
+            }
+        })
+
+    const width = 200;
+    const hoverSizeIncrease = 1.1;
+    const radius = width / (2 * hoverSizeIncrease);
+    const translate = width / 2;
+
+    const svg = d3.select("#borough-crime-pie-chart")
+        .attr("width", width)
+        .attr("height", width)
+        .append("g")
+        .attr("transform", `translate(${translate}, ${translate})`);
+
+    const color = d3.scaleOrdinal()
+        .domain(processedBoroughData.map(d => d.offence))
+        .range(d3.schemePaired);
+
+    const pie = d3.pie()
+        .value(d => d.count);
+
+    const arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+    const hoverArc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius * hoverSizeIncrease)
+
+    svg.selectAll("path")
+        .data(pie(processedBoroughData))
+        .enter()
+        .append("path")
+        .attr("d", arc)
+        .attr("fill", d => color(d.data.offence))
+        .on("mouseover", function () {
+            d3.select(this).transition()
+                .duration(100)
+                .ease(d3.easeQuadOut)
+                .attr("d", hoverArc);
+        }).on("mouseout", function () {
+            d3.select(this).transition()
+                .duration(400)
+                .ease(d3.easeBounceOut)
+                .attr("d", arc);
+        })
 }
 
 // noinspection JSIgnoredPromiseFromCall

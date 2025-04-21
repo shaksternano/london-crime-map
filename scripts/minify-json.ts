c0a48eda0d2c0522b422c1925d72387294152012
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-04-21 17:00:39 +0100

    perf: minify json

---
+++ scripts/minify-json.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+import * as path from "path";
+import viteConfig from "../vite.config";
+
+const OUTPUT_DIRECTORY = path.join(viteConfig.root, viteConfig.build.outDir);
+
+function minifyJsonFiles(directory: string) {
+    fs.readdirSync(directory).forEach(file => {
+        const filePath = path.join(directory, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            minifyJsonFiles(filePath);
+        } else if (path.extname(file) === ".json") {
+            const content = fs.readFileSync(filePath, "utf8");
+            const minifiedContent = minifyJson(content);
+            const outputPath = path.join(OUTPUT_DIRECTORY, file);
+            fs.writeFileSync(outputPath, minifiedContent + "\n");
+            console.log(`Minified: ${filePath}`);
+        }
+    });
+}
+
+function minifyJson(content: string): string {
+    return JSON.stringify(JSON.parse(content));
+}
+
+minifyJsonFiles(viteConfig.root);

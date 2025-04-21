36d9b02ac9834101f3cb2b33e1634492f3e19e45
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-04-21 18:27:20 +0100

    build: minify html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import {defineConfig} from "vite";
+import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vitejs.dev/config
 export default defineConfig({
+    plugins: [
+        createHtmlPlugin({
+            minify: true,
+        }),
+    ],
     root: "./src",
     publicDir: "../public/",
     build: {

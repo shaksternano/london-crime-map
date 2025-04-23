9a9d9224dcdddbf2113331d37fd13f34817ce494
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-04-23 12:00:18 +0100

    build: preserve source map

--- scripts/bundle-d3-plugin.ts
+++ scripts/bundle-d3-plugin.ts
@@ -4,14 +4,17 @@ export function bundleD3Plugin(): Plugin {
     return {
         name: "bundle-d3-plugin",
         transform(code, id) {
+            let transformedCode = code;
             if (id.endsWith(".js")) {
-                return code.replace(
+                transformedCode = code.replace(
                     /import\s+\*\s+as\s+d3\s+from\s+['"].+['"]/,
                     "import * as d3 from \"d3\"",
                 );
-            } else {
-                return code;
             }
+            return {
+                code: transformedCode,
+                map: null,
+            };
         },
     };
 }
